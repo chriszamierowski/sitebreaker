74accf12f109289c3c5e4974f9d86b7dddabd841
Chris Zamierowski <chriszamierowski@gmail.com>
2016-02-18 12:06:56 -0700

    adding basic main loop

--- src/scripts/sitebreaker/game.js
+++ src/scripts/sitebreaker/game.js
@@ -1,5 +1,6 @@
 let Util = require("./util");
 let StateMachine = require("./../../../node_modules/javascript-state-machine/state-machine.min");
+let MainLoop = require("./../../../node_modules/mainloop.js/build/mainloop.min");
 
 export default class Game {
   constructor () {
@@ -12,7 +13,22 @@ export default class Game {
     
     this.context = this.canvas.getContext('2d');
 
-    this.stateMachine = StateMachine.create({
+    this.stateMachine = this.setupStateMachine();
+
+    this.loop = this.setupLoop();
+
+    this.startLoop();
+  }
+
+  setupCanvas() {
+    let canvas = document.createElement('canvas');
+    canvas.className = 'sitebreaker-canvas';
+    document.body.appendChild(canvas);
+    return canvas;
+  }
+
+  setupStateMachine() {
+    return StateMachine.create({
       initial: 'menu',
       events: [
         { name: 'play', from: 'menu', to: 'game' },
@@ -36,10 +52,25 @@ export default class Game {
     });
   }
 
-  setupCanvas() {
-    let canvas = document.createElement('canvas');
-    canvas.className = 'sitebreaker-canvas';
-    document.body.appendChild(canvas);
-    return canvas;
+  setupLoop() {
+    return MainLoop
+      .setUpdate((d) => this.updateLoop(d))
+      .setDraw((d) => this.drawLoop(d))
+      .setEnd((d) => this.endLoop(d));
+  }
+
+  startLoop() {
+    this.loop.start();
+  }
+
+  updateLoop(delta) {
+    // console.log('update');
+  }
+
+  drawLoop(delta) {
+    // console.log('draw');
+  }
+
+  endLoop(delta) {
   }
 }
